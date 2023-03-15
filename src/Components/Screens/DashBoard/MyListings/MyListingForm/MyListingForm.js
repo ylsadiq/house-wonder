6c3279b6e499027af22f5ac295a6054c14c50482
greeenOrange <gm1sadiq@gmail.com>
2023-03-15 05:07:00 -0700

    my listion

--- src/Components/Screens/DashBoard/MyListings/MyListingForm/MyListingForm.js
+++ src/Components/Screens/DashBoard/MyListings/MyListingForm/MyListingForm.js
@@ -10,7 +10,33 @@ import FourForm from './FourForm/FourForm';
 
 function MyListingForm() {
     
+    // form field states
+    const [properyTypeCityArea, setProperyTypeCityArea] = useState(null);
+    const [ownerAcquisition, setOwnerAcquisition] = useState(null);
+    const [userPostAs, setUserPostAs] = useState(null);
+    const [acquisition, setAcquisition] = useState(null);
+    const [propertyPerspective, setpropertyPerspective] = useState(null);
+    const [propertyHeader, setPropertyHeader] = useState(null);
+    const [readyToMove, setReadyToMove] = useState(null);
+    const [propertyStreetAddress, setPropertyStreetAddress] = useState(null);
+    const [PropertyLandArea, setPropertyLandArea] = useState(null);
+    const [PropertyAddress, setPropertyAddress] = useState(null);
+    const [propertyAvailavleFrom, setPropertyAvailavleFrom] = useState(null);
+    const [propertyFlat, setPropertyFlat] = useState(null);
+    const [propertyHouse, setPropertyHouse] = useState(null);
+    const [propertyRoad, setPropertyRoad] = useState(null);
+    const [propertyTotalFlat, setPropertyTotalFlat] = useState(null);
+    const [contactPersonFlat, setContactPersonFlat] = useState(null);
+    const [postCode, setPostCode] = useState(null);
+    const [propertyDescription, setPropertyDescription] = useState(null);
+    const [videoUrl, setVideoUrl] = useState(null);
+    const [contactWithMe, setContactWithMe] = useState(null);
+    const [contactPersonName, setContactPersonName] = useState(null);
+    const [contactPersonRelation, setContactPersonRelation] = useState(null);
+    const [contactPersonEmail, setContactPersonEmail] = useState(null);
+    const [contactPersonPhone, setContactPersonPhone] = useState(null);
 
+    // form steps section
     const [formStep, setFormStep] = useState(1);
     const MAX_STEPS = 4;
 
@@ -34,17 +60,42 @@ function MyListingForm() {
                 </div>
             }
 
-            {formStep >= 2 && <div className={formStep === 2 ? 'd-block': 'd-none'}>
-            <SecondForm formStep={formStep} completeFormStep={completeFormStep} goToPreStep={goToPreStep}/>
-            </div> }
-            
-            {formStep >= 3 && <div className={formStep === 3 ? 'd-block': 'd-none'}>
-            <ThirdForm formStep={formStep} completeFormStep={completeFormStep} goToPreStep={goToPreStep} />
-            </div> }
+            {
 
-            {formStep >= 4 && <div className={formStep === 4 ? 'd-block': 'd-none'}>
-                <FourForm formStep={formStep} completeFormStep={completeFormStep} goToPreStep={goToPreStep}/>
-            </div> }
+                formStep >= 2 && 
+                <div className={formStep === 2 ? 'd-block': 'd-none'}>
+                    <SecondForm 
+                        formStep={formStep} 
+                        completeFormStep={completeFormStep} 
+                        goToPreStep={goToPreStep}
+                    />
+                </div> 
+            
+            }
+            
+            {
+            
+                formStep >= 3 && 
+                <div className={formStep === 3 ? 'd-block': 'd-none'}>
+                    <ThirdForm 
+                        formStep={formStep} 
+                        completeFormStep={completeFormStep}
+                        goToPreStep={goToPreStep} 
+                    />
+                </div> 
+            
+            }
+
+            {
+                formStep >= 4 && 
+                <div className={formStep === 4 ? 'd-block': 'd-none'}>
+                    <FourForm 
+                        formStep={formStep} 
+                        completeFormStep={completeFormStep} 
+                        goToPreStep={goToPreStep}
+                    />
+                </div> 
+            }
         </>
     )
 }
