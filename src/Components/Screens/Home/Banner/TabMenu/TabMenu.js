0b9d3860649be6cd7705d239a4f520d8b6dbf850
Raihan Kabir <raihancse46@gmail.com>
2023-03-22 12:11:33 +0600

    22032023 updates

--- src/Components/Screens/Home/Banner/TabMenu/TabMenu.js
+++ src/Components/Screens/Home/Banner/TabMenu/TabMenu.js
@@ -12,47 +12,46 @@ function TabMenu() {
     const toggleExpandFilter = () => {
         setExpandFilter(pervState => !pervState)
     }
-   
+
     return (
         <div className="property_filter_wrapper">
             <div className="property_filter_tabs">
                 <button onClick={() => toggleTab(1)} className={toggleState === 1 ? 'property_filter_tab active' : 'property_filter_tab'}>Buy</button>
                 <button onClick={() => toggleTab(2)} className={toggleState === 2 ? 'property_filter_tab active' : 'property_filter_tab'}>Rent</button>
             </div>
-            
+
             {toggleState && <div className={`property_filter_search ${expandFilter ? 'expanded' : null}`}>
                 <form className="flex_container">
-                    <div className='flex_item search-expand-btn'>
+                    <div className='flex_item'>
                         <div className="property_search_box">
                             <input type="email" className='search_input' placeholder='Search for city, area, ZIP...' />
                             <img src={searchIcon} alt="" />
+                        </div>
+                        <div className='search-expand-btn'>
+                            <img src={filter} alt="" onClick={toggleExpandFilter} />
                         </div>
                     </div>
                     <div className='flex_item'>
                         <div className="filter_select_group">
                             <div className="grid_container form_search">
-                                {expandFilter &&
-                                    <>
-                                        <div className='grid_item'>
-                                            <label>Property Type</label>
-                                            <select className='form_select'>
-                                                <option value="">Select</option>
-                                                <option value="">P type 1</option>
-                                                <option value="">P type 2</option>
-                                                <option value="">P type 3</option>
-                                            </select>
-                                        </div>
-                                        <div className='grid_item'>
-                                            <label>Property Sub-Type</label>
-                                            <select className='form_select'>
-                                                <option value="">Select</option>
-                                                <option value="">P Sub-Type 1</option>
-                                                <option value="">P Sub-Type 2</option>
-                                                <option value="">P Sub-Type 3</option>
-                                            </select>
-                                        </div>
-                                    </>
-                                }
+                                <div className='grid_item'>
+                                    <label>Property Type</label>
+                                    <select className='form_select'>
+                                        <option value="">Select</option>
+                                        <option value="">P type 1</option>
+                                        <option value="">P type 2</option>
+                                        <option value="">P type 3</option>
+                                    </select>
+                                </div>
+                                <div className='grid_item'>
+                                    <label>Property Sub-Type</label>
+                                    <select className='form_select'>
+                                        <option value="">Select</option>
+                                        <option value="">P Sub-Type 1</option>
+                                        <option value="">P Sub-Type 2</option>
+                                        <option value="">P Sub-Type 3</option>
+                                    </select>
+                                </div>
                                 <div className="grid_item">
                                     <img src={filter} alt="" onClick={toggleExpandFilter} />
                                     <button type='button' className='btn dark_btn search_btn'>search</button>
